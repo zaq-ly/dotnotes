3631025a7e07e9a95777ee8ef4723073b0a1bbbb
Muhammad Zaqly Luluang <muhammadzaqly01@gmail.com>
2026-08-20 01:33:18 +0800

    chore(release): v1.0.4 - Perbaikan nomor versi internal APK dan update logo no-crop

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.dotnotes.app"
         minSdk = flutter.minSdkVersion
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
         multiDexEnabled = true
     }
 
