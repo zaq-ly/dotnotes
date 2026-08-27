5afc748f1ab874b32a95f54fb581611a55cfe2ea
Muhammad Zaqly Luluang <muhammadzaqly01@gmail.com>
2026-08-27 23:14:55 +0800

    style: update app icon logo (Release v1.0.6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.dotnotes.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
     }
 
     buildTypes {
