ea86aea8222411d94eebd1916d8354b42ef15d89
Muhammad Zaqly Luluang <muhammadzaqly01@gmail.com>
2026-08-27 15:46:53 +0800

    chore: clean slate - remove all legacy files

--- android/app/src/main/kotlin/com/dotnotes/dotnotes/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.dotnotes.dotnotes
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/constants/app_constants.dart
+++
@@ -1,7 +0,0 @@
-abstract class AppConstants {
-  static const appName = '.notes';
-  static const googleDriveBackupFilename = 'notes_backup.json';
-  static const prefThemeModeKey = 'pref_theme_mode';
-  static const prefLocaleKey = 'pref_locale';
-  static const prefLastSyncedKey = 'pref_last_synced';
-}
